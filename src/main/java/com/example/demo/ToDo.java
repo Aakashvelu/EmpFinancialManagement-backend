9f4cc56a7feaf335cc70015802bf3a906355deb2
Aakashvelu <aakashvelu11@gmail.com>
2023-02-27 07:38:10 +0530

    first commit

---
+++ src/main/java/com/example/demo/ToDo.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ToDo {
+	@Id
+	private int id;
+	private String name;
+	private String isCompleted;
+	public ToDo() {
+		super();
+		
+	}
+	public ToDo(int id,String name,String isCompleted) {
+		super();
+		this.id=id;
+		this.name=name;
+		this.isCompleted=isCompleted;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIsCompleted() {
+		return isCompleted;
+	}
+	public void setIsCompleted(String isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+
+}
